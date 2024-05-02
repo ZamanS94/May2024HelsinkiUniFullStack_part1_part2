9070ce227f0bc1be6c8e9b9955c27cf68d10ca1c
Sabina Zaman <sabina.zaman94@gmail.com>
2024-05-02 20:35:21 +0200

    1.2: Course Information, step 2

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -10,12 +10,20 @@ const Header = (props) => {
   )
 }
 
+const Part = (props) => {
+  return (
+    <div>
+      <p>{props.part} {props.exercises}</p>
+    </div>
+  )
+}
+
 const Content = (props) => {
   return (
     <div>
-      <p>
-      {props.part} {props.exercise}
-      </p>
+    <Part part = {props.part1} exercises = {props.exercises1}/>
+    <Part part = {props.part2} exercises= {props.exercises2}/>
+    <Part part = {props.part3} exercises = {props.exercises3}/>
     </div>
   )
 }
@@ -24,7 +32,7 @@ const Total = (props) => {
   return (
     <div>
       <p>
-      Number of exercises {props.total}
+      Number of exercises {props.totalCount}
       </p>
     </div>
   )
@@ -41,11 +49,14 @@ const App = () => {
 
   return (
     <div>
-      <Header course = {course}/> 
-      <Content part = {part1} exercise = {exercises1}/>
-      <Content part = {part2} exercise = {exercises2}/>
-      <Content part = {part3} exercise = {exercises3}/>
-      <Total total = {exercises1 + exercises2 + exercises3} />
+      <Header course={course}/> 
+      <Content 
+        part1 = {part1} exercises1 = {exercises1}
+        part2 = {part2} exercises2 = {exercises2}
+        part3 = {part3} exercises3 = {exercises3}
+      />
+      <Total totalCount = {exercises1 + exercises2 + exercises3} />
+     
     </div>
   )
 }
