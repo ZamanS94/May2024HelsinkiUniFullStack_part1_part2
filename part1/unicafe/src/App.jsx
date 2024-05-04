4ecb73a48ead38bb86cb7da3013f1538b81e7a07
Sabina Zaman <sabina.zaman94@gmail.com>
2024-05-04 21:02:31 +0200

    1.7: unicafe step 2

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -5,20 +5,24 @@ const App = () => {
   const [goodClick, setGood] = useState({good : 0})
   const [neutralClick, setNeutral] = useState({neutral : 0})
   const [badClick, setBad] = useState({bad : 0})
+  const [total, setTotal] = useState(0)
 
   const handleGoodClick = () => {
     const newGood = { ...goodClick, good: goodClick.good + 1 }
     setGood(newGood)
+    setTotal(newGood.good + neutralClick.neutral + badClick.bad )
   }
-  
+
   const handleNeutralClick = () => {
     const newNeutral = { ...neutralClick, neutral: neutralClick.neutral + 1 }
     setNeutral(newNeutral)
+    setTotal(newNeutral.neutral + goodClick.good + badClick.bad )
   }
 
   const handleBadClick = () => {
     const newBad = { ...badClick, bad: badClick.bad + 1 }
     setBad(newBad)
+    setTotal(newBad.bad + neutralClick.neutral + goodClick.good )
   }
 
   return (
@@ -31,6 +35,9 @@ const App = () => {
       <p>good {goodClick.good}</p>
       <p>neutral {neutralClick.neutral}</p>
       <p>bad {badClick.bad}</p>
+      <p>all {total}</p>
+      <p>average {(goodClick.good-badClick.bad)/total}</p>
+      <p>positive {goodClick.good/total*100}%</p>
     </div>
   )
 }
