00e57e4007bbe9ed9ed13709230feb7a5cea7e9f
Sabina Zaman <sabina.zaman94@gmail.com>
2024-05-07 03:56:34 +0200

    Exercise 2.11.

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,19 +1,26 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Person from './components/Person.jsx'
 import PersonForm from './components/PersonForm.jsx'
 import Filter from './components/Filter.jsx'
 import _ from 'lodash'
+import axios from 'axios'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [newSearch, setSearch] = useState('')
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }, [])
+  console.log('render', persons.length, 'persons')
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -22,7 +29,7 @@ const App = () => {
       number: newNumber,
       id: Math.floor(Math.random() * (100 - persons.length)) + persons.length + 1
     }
-    const doesExist = persons.find(person => _.isEqual(person, personObject))
+    const doesExist = persons.find(person => person.name === newName && person.number === newNumber)
 
     if(doesExist){
       window.alert(`${doesExist.name} is already added to phonebook`)
