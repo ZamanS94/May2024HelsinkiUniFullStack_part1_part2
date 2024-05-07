44b2cd015562c3fece30b5373f4f646bc154267e
Sabina Zaman <sabina.zaman94@gmail.com>
2024-05-07 02:33:44 +0200

    2.9*: The Phonebook Step 4

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -4,18 +4,21 @@ import _ from 'lodash'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas',
-      number: '040-123'
-     }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [newSearch, setSearch] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
     const personObject = {
       name: newName,
-      number: newNumber
+      number: newNumber,
+      id: Math.floor(Math.random() * (100 - persons.length)) + persons.length + 1
     }
     const doesExist = persons.find(person => _.isEqual(person, personObject))
 
@@ -39,20 +42,31 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const handlePersonSearch= (event) => {
+    setSearch(event.target.value)
+  }
 
+  const searchResult = persons.filter(person => 
+    person.name.toLocaleLowerCase().includes(newSearch.toLowerCase()))
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <form> 
+        <div>filter shown with <input value={newSearch} onChange={handlePersonSearch}/></div>
+        </form>
+        <div>
+          {searchResult.map(person => <Person key={person.id} person={person} />)}
+          </div>
+          <h2>add a new</h2>
       <form onSubmit={addPerson}>
   <div>name: <input value={newName} onChange={handleNameChange}/></div>
   <div>number: <input value={newNumber} onChange={handleNumberChange}/></div>
   <div><button type="submit">add</button></div>
 </form>
-      <h2>Numbers</h2>
-        {persons.map(person => 
-          <Person key={person.name} person={person} />
-        )}
+<h2>Numbers</h2>
+{persons.map(person => 
+<Person key={person.id} person={person} />)}
     </div>
   )
 }
